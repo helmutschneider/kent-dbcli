676707ce93cc013687e7695e0900ddb4912d15fa
Johan Björk <johanimon@gmail.com>
2024-04-06 23:38:39 +0200

    Fix the broken build script when cross-compiling

--- build.sh
+++ build.sh
@@ -17,11 +17,12 @@ mkdir -p build/
 for RUNTIME in "osx-arm64" "win-x64" "linux-x64"; do
   BUILD_NAME="kent-dbcli-${VERSION}-${RUNTIME}"
   BUILD_DIR="build/${BUILD_NAME}"
+  dotnet clean
   dotnet publish \
-    -c Release \
+    --configuration Release \
     --no-cache \
     --self-contained \
     --runtime "${RUNTIME}" \
     --output "${BUILD_DIR}"
-  tar -czf "build/${BUILD_NAME}.tar.gz" -C "${BUILD_DIR}" .
+  tar --exclude=".[^/]*" -czf "build/${BUILD_NAME}.tar.gz" -C "${BUILD_DIR}" .
 done
